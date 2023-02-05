eed5e8a432e96a9ba070957b62d2b0c5f2c3a661
Marcela Alvarez <rezmos@gmail.com>
2023-02-05 17:41:40 -0500

    Adding ml model: testing and training

--- src/main.py
+++ src/main.py
@@ -71,7 +71,7 @@ numeric_dtypes_corr = df_diabetes.corr()
 sns.heatmap(numeric_dtypes_corr, annot=True)
 plt.show()
 
-#Cleaning:
+#3. Cleaning:
 
 
 df_diabetes["gender"] = df_diabetes["gender"].replace("Unknown/Invalid", np.NaN)
@@ -82,9 +82,21 @@ df_diabetes.dropna(subset=["race"], axis=0, inplace=True)
 
 cu.distribution_by_category(var_cat,df_diabetes)
 
-#NOrmaliza?
-df_diabetes_target_prediction_variables = pd.get_dummies(df_diabetes[[ 'race', 'gender', 'diabetesMed', 'num_procedures',
-		 'admission_type_id', 'readmitted_num','time_in_hospital', 'num_medications', 'num_lab_procedures', 'number_diagnoses', 'number_inpatient', 'discharge_disposition_id']])
+'''
+# 4. Dataset preparation
+I selected K Nearest Neighbor algorithm because is really simple, easy to understand and one of the topmost machine learning algorithms. This KNN Classifier needs the training set and the test set.
+To obtain those sets, I needed to split the data: training set size 70% and test set size 30%. Right no
+'''
+df_diabetes_training = df_diabetes[:69646]
+df_diabetes_test = df_diabetes[69646:]
+# 5.  Training and Evaluating the Machine Learning Model
+# The next step is grouping all features/variables with significant correlation with the outcome variable like:
+# number_inpatient,number_emergency,number_diagnoses, race, gender, diabetesMed and admission_type_id
+# And variables with correlation between them like:
+#  time_in_hospital, num_medications, num_lab_procedures, num_procedures, number_inpatient, number_outpatient and discharge_disposition_id
+# This list of variables correspond to the variables that helps to predict the model
+df_diabetes_target_prediction_variables = pd.get_dummies(df_diabetes_training[[ 'patient_nbr', 'race', 'gender', 'diabetesMed', 'number_inpatient', 'number_emergency', 'number_diagnoses', 'admission_type_id',
+		 'time_in_hospital','num_medications','num_lab_procedures','num_procedures',  'number_outpatient', 'discharge_disposition_id', 'readmitted_num']])
 print(df_diabetes_target_prediction_variables.head())
 
 x = df_diabetes_target_prediction_variables
@@ -100,3 +112,15 @@ lm_model.fit(X_train, y_train)
 
 y_pred = lm_model.predict(X_test)
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+df_diabetes_test_t = pd.get_dummies(df_diabetes_test[[ 'patient_nbr','race', 'gender', 'diabetesMed', 'number_inpatient', 'number_emergency', 'number_diagnoses', 'admission_type_id',
+		 'time_in_hospital','num_medications','num_lab_procedures','num_procedures',  'number_outpatient', 'discharge_disposition_id']])
+print(df_diabetes_test_t.head())
+# Generate test predictions
+preds_test = lm_model.predict(df_diabetes_test_t)
+
+
+# Save predictions in format used for competition scoring
+output = pd.DataFrame({'Id': df_diabetes_test_t.patient_nbr,
+                       'readmitted': preds_test})
+output.to_csv('readmitted_T_KNN_v2.csv', index=False)
